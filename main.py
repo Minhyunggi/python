aa660226b8a4e8431685238bb826b457e1a88bca
민형기 <alsvhtks@naver.com>
2023-01-04 13:41:02 +0900

    for

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-player = {
-    'name' : "min",
-    'age' : 27,
-    'alive' : True
-}
-print(player.get("name"))
+websites = (
+    "https://google.com",
+    "airbnb.com",
+    "https://facebook.com",
+    "twitter.com",
+    "tiktok.com"
+)
+
+for website in websites:
+    if not website.startswith("https://"):
+        website = f"https://{website}"
+print(websites)
