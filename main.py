76779d1ac2ad163382d3aebc1d52961d95d8e412
민형기 <alsvhtks@naver.com>
2023-01-04 12:36:45 +0900

    터플 리스트 딕셔너리

--- main.py
+++ main.py
@@ -1,24 +1,6 @@
-import random
-
-print("Welcome to python casino")
-pc_choice = random.randint(0,50)
-playing = True
-while playing:
-    user_choice = int(input("Choose number"))
-    if user_choice == pc_choice:
-        print("You win")
-        playing = False
-    elif user_choice > pc_choice:
-        print("Lower")
-    elif user_choice < pc_choice:
-        print("Higher")
-
-
-
-
-
-
-# distance = 0
-# while distance < 20:
-#     print("i'm running" , distance ,"km")
-#     distance = distance + 1
+player = {
+    'name' : "min",
+    'age' : 27,
+    'alive' : True
+}
+print(player.get("name"))
